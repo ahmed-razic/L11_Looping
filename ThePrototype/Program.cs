c9e82ecf6e32e9160959f94471f4e16207d46373
ahmed-razic <ahmed.razic@gmail.com>
2026-07-19 09:55:13 +0200

    commit

--- ThePrototype/Program.cs
+++ ThePrototype/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            
+            int numberToGuess;
+            int number;
+
+            do
+            {
+                Console.Write("User 1, enter a number between 1 and 100: ");
+                numberToGuess = int.Parse(Console.ReadLine());
+            } while (numberToGuess > 100 || numberToGuess < 0);
+
+            Console.Clear();
+
+            do
+            {
+                Console.Write("User 2, guess the number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number > numberToGuess)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else if (number < numberToGuess)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+
+            } while (number != numberToGuess);
+
+            Console.WriteLine("Congratulations! You guessed the number! It was {0}.", numberToGuess);
         }
     }
 }
